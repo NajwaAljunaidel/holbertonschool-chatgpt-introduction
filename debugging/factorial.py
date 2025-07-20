585d05ea9e93682d7ca90b540275329693dcfd89
Najwa <najwa.aljunaidel@gmail.com>
2025-07-20 11:41:00 +0300

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,10 +2,10 @@
 import sys
 
 def factorial(n):
-    result = 1 
+    result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # 👈 مهم جداً لتقليل قيمة n
     return result
 
 f = factorial(int(sys.argv[1]))
