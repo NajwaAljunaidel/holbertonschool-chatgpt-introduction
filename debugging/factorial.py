d8255f3c07bcc6b7d1702048d2101b6d094da3ee
Najwa <najwa.aljunaidel@gmail.com>
2025-07-20 11:47:30 +0300

    Update factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,7 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # 👈 إصلاح الخطأ: تقليل n داخل الحلقة
+        n -= 1
     return result
 
 f = factorial(int(sys.argv[1]))
